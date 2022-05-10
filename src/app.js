3e974035a4ed0ce3c054b36e2bd01dc707c0596f
Mau-Me <mauriciosm95@gmail.com>
2022-05-10 18:07:43 -0300

    Add: Implementa CRUD em memória utilizando array

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
   {
     id: 1,
@@ -20,5 +22,43 @@ app.get("/", (req, res) => {
 app.get("/livros", (req, res) => {
   res.json(livros);
 });
+
+app.get("/livros/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const indiceLivro = buscaIdLivro(id);
+
+  res.json(livros[indiceLivro]);
+});
+
+app.post("/livros", (req, res) => {
+  const livro = req.body;
+  livros.push(livro);
+
+  res.status(201).end();
+});
+
+app.put("/livros/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const novoLivro = req.body;
+
+  const indiceLivro = buscaIdLivro(id);
+
+  livros[indiceLivro].nome = novoLivro.nome;
+
+  res.end();
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const indiceLivro = buscaIdLivro(id);
+  livros.splice(indiceLivro, 1);
+
+  res.end();
+});
+
+function buscaIdLivro(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
 
 export default app;
