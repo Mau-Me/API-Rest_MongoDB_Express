40e3b470186a5faebbeaf3937070e71aba2bac9b
Mau-Me <mauriciosm95@gmail.com>
2022-05-11 05:34:09 -0300

    Add: autoresRoutes ao agregador de rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import livrosRoutes from "./livrosRoutes.js";
+import autoresRoutes from "./autoresRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send({ Titulo: "Curso de Node" });
   });
 
-  app.use(express.json(), livrosRoutes);
+  app.use(express.json(), livrosRoutes, autoresRoutes);
 };
 
 export default routes;
