d9e58b74efefe77aa382379224f8296487445496
Mau-Me <mauriciosm95@gmail.com>
2022-05-11 03:50:14 -0300

    Refactor: Remove atribuições do controller/router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "Erro de Conexão"));
 db.once("open", () => {
@@ -9,54 +9,6 @@ db.once("open", () => {
 
 const app = express();
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Curso de Node");
-});
-
-app.get("/livros", (req, res) => {
-  livros.find((err, livros) => {
-    res.json(livros);
-  });
-});
-
-app.get("/livros/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const indiceLivro = buscaIdLivro(id);
-
-  res.json(livros[indiceLivro]);
-});
-
-app.post("/livros", (req, res) => {
-  const livro = req.body;
-  livros.push(livro);
-
-  res.status(201).end();
-});
-
-app.put("/livros/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const novoLivro = req.body;
-
-  const indiceLivro = buscaIdLivro(id);
-
-  livros[indiceLivro].nome = novoLivro.nome;
-
-  res.end();
-});
-
-app.delete("/livros/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const indiceLivro = buscaIdLivro(id);
-  livros.splice(indiceLivro, 1);
-
-  res.end();
-});
-
-function buscaIdLivro(id) {
-  return livros.findIndex((livro) => livro.id == id);
-}
+routes(app);
 
 export default app;
